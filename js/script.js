aba4ed381c5e0d5260bfbc7759e8aded8e0fde51
anderson.batista <andersonwdf@gmail.com>
2021-08-29 23:23:38 -0300

    Adicionando Modificações em Habilidades

--- js/script.js
+++ js/script.js
@@ -26,3 +26,45 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+//----------Habilidades-------
+const skillsContent = document.getElementsByClassName('skills-content');
+const skillsHeader = document.querySelectorAll('.skills-header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills-content skills-close';
+  }
+  if (itemClass === 'skills-content skills-close') {
+    this.parentNode.className = 'skills-content skills-open';
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
+
+////////// Modal/////////////
+const modalViews = document.querySelectorAll('.services-modal');
+const modalBtns = document.querySelectorAll('.services-button');
+const modalCloses = document.querySelectorAll('.services-modal-close');
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtns, i) => {
+  modalBtns.addEventListener('click', () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal');
+    });
+  });
+});
