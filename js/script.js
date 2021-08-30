eed660aec3341847961edae72f2c042888cd24c0
anderson.batista <andersonwdf@gmail.com>
2021-08-30 14:50:14 -0300

    Modificando Seção Projetos

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills);
 });
 
-////////// Modal/////////////
+//////// Modal///////////
 const modalViews = document.querySelectorAll('.services-modal');
 const modalBtns = document.querySelectorAll('.services-button');
 const modalCloses = document.querySelectorAll('.services-modal-close');
@@ -68,3 +68,42 @@ modalCloses.forEach((modalClose) => {
     });
   });
 });
+
+///////Portfolio swiper////
+let swiper = new Swiper('.portfolio-container', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+///// Links scroll //////
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav-menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav-menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
